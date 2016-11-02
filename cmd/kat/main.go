c4f32e35cfea2712388ceacc6fb50d542612e9ab
Martin Czygan <martin.czygan@gmail.com>
2016-11-02 11:39:52 +0100

    update docs

--- cmd/kat/main.go
+++ cmd/kat/main.go
@@ -26,18 +26,23 @@ doc, docx, xlsx, tar, tar.gz, dmg, djvu.
 $ kat FILE
 `
 
+// Version.
 const Version = "0.1.2"
 
+// ErrUnsupportedFiletype, when file
 var ErrUnsupportedFiletype = errors.New("unsupported file type")
 
+// Viewer emits a view of a thing.
 type Viewer interface {
 	View() ([]byte, error)
 }
 
+// File, something that has name.
 type File struct {
 	Name string
 }
 
+// View is the generic via cat.
 func (f *File) View() ([]byte, error) {
 	if _, err := exec.LookPath("cat"); err != nil {
 		return nil, fmt.Errorf("cat is required")
@@ -45,10 +50,12 @@ func (f *File) View() ([]byte, error) {
 	return exec.Command("cat", f.Name).Output()
 }
 
+// PDF file.
 type PDF struct {
 	File
 }
 
+// View PDF as text.
 func (f *PDF) View() ([]byte, error) {
 	if _, err := exec.LookPath("pdftotext"); err != nil {
 		return nil, fmt.Errorf("pdftotext is required")
@@ -56,10 +63,12 @@ func (f *PDF) View() ([]byte, error) {
 	return exec.Command("pdftotext", f.Name, "-").Output()
 }
 
+// Image file.
 type Image struct {
 	File
 }
 
+// View image with catimg.
 func (f *Image) View() ([]byte, error) {
 	if _, err := exec.LookPath("catimg"); err != nil {
 		return nil, fmt.Errorf("catimg is required")
@@ -67,10 +76,12 @@ func (f *Image) View() ([]byte, error) {
 	return exec.Command("catimg", "-w", "192", f.Name).Output()
 }
 
+// BinaryMARC21 bibliographic data.
 type BinaryMARC21 struct {
 	File
 }
 
+// View with yaz.
 func (f *BinaryMARC21) View() ([]byte, error) {
 	if _, err := exec.LookPath("yaz-marcdump"); err != nil {
 		return nil, fmt.Errorf("yaz-marcdump is required")
@@ -78,10 +89,12 @@ func (f *BinaryMARC21) View() ([]byte, error) {
 	return exec.Command("yaz-marcdump", f.Name).Output()
 }
 
+// Zipfile, compressed file.
 type Zipfile struct {
 	File
 }
 
+// View lists zip file contents.
 func (f *Zipfile) View() ([]byte, error) {
 	if _, err := exec.LookPath("unzip"); err != nil {
 		return nil, fmt.Errorf("unzip is required")
@@ -89,21 +102,25 @@ func (f *Zipfile) View() ([]byte, error) {
 	return exec.Command("unzip", "-l", f.Name).Output()
 }
 
-type TGZ struct {
+// Tarfile archive.
+type Tarfile struct {
 	File
 }
 
-func (f *TGZ) View() ([]byte, error) {
+// View lists tar contents.
+func (f *Tarfile) View() ([]byte, error) {
 	if _, err := exec.LookPath("tar"); err != nil {
 		return nil, fmt.Errorf("tar is required")
 	}
 	return exec.Command("tar", "tf", f.Name).Output()
 }
 
+// MP3 audio.
 type MP3 struct {
 	File
 }
 
+// View display file information.
 func (f *MP3) View() ([]byte, error) {
 	if _, err := exec.LookPath("mp3info"); err != nil {
 		return nil, fmt.Errorf("mp3info is required")
@@ -111,10 +128,12 @@ func (f *MP3) View() ([]byte, error) {
 	return exec.Command("mp3info", "-x", f.Name).Output()
 }
 
+// Rar archive.
 type Rar struct {
 	File
 }
 
+// View lists rar contents.
 func (f *Rar) View() ([]byte, error) {
 	if _, err := exec.LookPath("unrar"); err != nil {
 		return nil, fmt.Errorf("unrar is required")
@@ -122,10 +141,12 @@ func (f *Rar) View() ([]byte, error) {
 	return exec.Command("unrar", "l", f.Name).Output()
 }
 
+// ODT document.
 type ODT struct {
 	File
 }
 
+// View extracts text.
 func (f *ODT) View() ([]byte, error) {
 	if _, err := exec.LookPath("docd"); err != nil {
 		return nil, fmt.Errorf("docd is required")
@@ -133,10 +154,12 @@ func (f *ODT) View() ([]byte, error) {
 	return exec.Command("docd", "-input", f.Name).Output()
 }
 
+// Word, traditional.
 type Word struct {
 	File
 }
 
+// View extracts text from Word.
 func (f *Word) View() ([]byte, error) {
 	if _, err := exec.LookPath("antiword"); err != nil {
 		return nil, fmt.Errorf("antiword is required")
@@ -144,10 +167,12 @@ func (f *Word) View() ([]byte, error) {
 	return exec.Command("antiword", f.Name).Output()
 }
 
+// XLSX spreadsheet.
 type XLSX struct {
 	File
 }
 
+// View lists the (first) spreadsheet.
 func (f *XLSX) View() ([]byte, error) {
 	// https://git.io/vXOHi
 	if _, err := exec.LookPath("xlsx2tsv.py"); err != nil {
@@ -160,10 +185,12 @@ func (f *XLSX) View() ([]byte, error) {
 	return exec.Command("xlsx2tsv.py", f.Name, "1").Output()
 }
 
+// DMG disk image.
 type DMG struct {
 	File
 }
 
+// View list info about the image.
 func (f *DMG) View() ([]byte, error) {
 	if _, err := exec.LookPath("hdiutil"); err != nil {
 		return nil, fmt.Errorf("hdiutil is required")
@@ -171,10 +198,12 @@ func (f *DMG) View() ([]byte, error) {
 	return exec.Command("hdiutil", "imageinfo", f.Name).Output()
 }
 
+// Djvu document.
 type Djvu struct {
 	File
 }
 
+// View extracts text.
 func (f *Djvu) View() ([]byte, error) {
 	if _, err := exec.LookPath("djvutxt"); err != nil {
 		return nil, fmt.Errorf("djvutxt is required")
@@ -182,6 +211,7 @@ func (f *Djvu) View() ([]byte, error) {
 	return exec.Command("djvutxt", f.Name).Output()
 }
 
+// DispatchFile chooses a viewer for a given filename.
 func DispatchFile(s string) (Viewer, error) {
 	switch {
 	case strings.HasSuffix(s, ".pdf"):
@@ -197,11 +227,11 @@ func DispatchFile(s string) (Viewer, error) {
 	case strings.HasSuffix(s, ".zip"):
 		return &Zipfile{File{Name: s}}, nil
 	case strings.HasSuffix(s, ".tgz"):
-		return &TGZ{File{Name: s}}, nil
+		return &Tarfile{File{Name: s}}, nil
 	case strings.HasSuffix(s, ".tar"):
-		return &TGZ{File{Name: s}}, nil
+		return &Tarfile{File{Name: s}}, nil
 	case strings.HasSuffix(s, ".tar.gz"):
-		return &TGZ{File{Name: s}}, nil
+		return &Tarfile{File{Name: s}}, nil
 	case strings.HasSuffix(s, ".mp3"):
 		return &MP3{File{Name: s}}, nil
 	case strings.HasSuffix(s, ".rar"):
